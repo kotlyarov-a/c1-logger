5d872ec657b67e295aa2124d2958e4545d951d29
kotlyarov-a <63169617+kotlyarov-a@users.noreply.github.com>
2021-08-30 15:04:29 +0300

    dateTime

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ app.get("/", (req, res) => {
 });
 
 app.get("/test", (req, res) => {
-    let dateTime = new Date().toISOString;
+    let dateTime = new Date().toISOString();
     console.log(`${dateTime}`);
     let content = "test"
     res.send(content)
 })
 
+
 const jsonParser = express.json();
 
 // let arr = [];
 app.post("/test", jsonParser, (req, res) => {
-    let dateTime = new Date().toISOString;
+    let dateTime = new Date().toISOString();
     console.log(`${dateTime} + ${req.body}`);
     // console.log(JSON.stringify(req.body));
     let payload = req.body;
